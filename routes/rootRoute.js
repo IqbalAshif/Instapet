395f533efa68b929865f201791e85244912ef3c1
Ashif Iqbal <ashif.iqbal@metropolia.fi>
2020-11-25 15:04:58 +0200

    first connection with the datatbase/user

--- routes/rootRoute.js
+++ routes/rootRoute.js
@@ -1,9 +1,13 @@
-'use strict'
+'use strict';
+//root routes (example with get, post and put)
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   console.log('rootRoute: root route with req: ', req.query);
+  res.send(
+    `Hello world <a href="cat">click</a> <br> with test is ${req.query.test} and fun is ${req.query.more}`
+  );
 });
 
 router.post('/', (req, res) => {
